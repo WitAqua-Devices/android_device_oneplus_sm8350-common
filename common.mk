fc98f492588c545d99353896d17b817ab40700b8
Pranav Vashi <neobuddy89@gmail.com>
2023-05-06 20:14:10 +0530

    sm8350-common: Move few kernel scripts to device tree
    * The oplus modifications are no-op. It's better to have working
      qcom implementation instead.
    * This also allows to further fine-tune.

--- common.mk
+++ common.mk
@@ -253,6 +253,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
